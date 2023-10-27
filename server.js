844415ad03c527b5f5ba34d90a8c35d7a14a8afe
ngel rojas <aomarrojasp@gmail.com>
2023-10-27 10:15:01 -0300

    add: adding start project

---
+++ server.js
@@ -0,0 +1,19 @@
+const app = require("express")();
+const server = require("http").Server(app);
+const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+require("dotenv").config({path: "./config.env"});
+const connectDB = require("./utilsServer/connectDb");
+const PORT = process.env.PORT || 3000;
+connectDB();
+
+nextApp.prepare().then(() => {
+    app.all("*", (req, res) => {
+        server.listen(PORT, err => {
+            if(err) throw err;
+            console.log(`Express server running on port ${PORT}`);
+        })
+    })
+});
